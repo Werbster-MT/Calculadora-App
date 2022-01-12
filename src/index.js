209472318b4672be746d63cce8bf10d6b3cf8c32
Werbster99 <wmarques.88@hotmail.com>
2022-01-12 00:23:18 -0300

    update

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/Calculator';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.Fragment>
+    <h1>Calculator</h1>
+    <Calculator/>
+  </React.Fragment>,  
+  document.getElementById('root')
+);
+
+reportWebVitals();
